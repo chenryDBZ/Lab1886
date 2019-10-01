e57ebc6229da97dadf2fadc5900bcb0db745770c
chenrydbz <carlton.alvin.henry@gmail.com>
2019-10-01 10:13:27 -0400

    Submit form test border commented out

--- js/main.js
+++ js/main.js
@@ -1,14 +1,6 @@
 $(document).ready(function(){
-  let selector = ".navbar li";
-
   $(".navbar li").click(function(){
     $(".navbar li").removeClass('active');
     $(this).addClass('active');
   });
 });
-
-$(document).ready(function() {
-  $("#lightSlider").lightSlider();
-});
-
-//$(".navbar ul").css({border: "3px solid"});
